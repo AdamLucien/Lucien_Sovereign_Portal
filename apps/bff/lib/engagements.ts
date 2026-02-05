8e8f170c1ddb2b24fdb85f1dab365ce7793effc9
Adam Karl Lucien <68617017+AdamLucien@users.noreply.github.com>
2026-02-05 13:53:46 +0100

    Fix engagement id type check

--- apps/bff/lib/engagements.ts
+++ apps/bff/lib/engagements.ts
@@ -4,7 +4,7 @@ export const TIER_ENGAGEMENT_MAP = {
   2: ['TIER-SOVEREIGN'],
 } as const;
 
-const ALL_ENGAGEMENT_IDS = new Set(Object.values(TIER_ENGAGEMENT_MAP).flat());
+const ALL_ENGAGEMENT_IDS = new Set<string>(Object.values(TIER_ENGAGEMENT_MAP).flat());
 
 export const isValidEngagementId = (value: string) => ALL_ENGAGEMENT_IDS.has(value);
 
